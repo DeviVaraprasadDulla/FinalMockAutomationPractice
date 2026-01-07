8064f3686a7bb1133cdff0e37058a96c0eb7809d
Devi varaprasad Dulla <devivaraprasaddulla@gmail.com>
2026-01-07 15:36:40 +0530

    added dependsOnMethods

---
+++ src/test/java/com/practice/TestNg/D_DataProvider.java
@@ -0,0 +1,5 @@
+package com.practice.TestNg;
+
+public class D_DataProvider {
+
+}
--- src/test/java/com/practice/TestNg/D_DependsOnMethod.java
+++ src/test/java/com/practice/TestNg/D_DependsOnMethod.java
@@ -5,24 +5,24 @@ import org.testng.annotations.Test;
 public class D_DependsOnMethod {
 	
 
-	@Test(priority=1)
+	@Test
 	public void registration() {
 		System.out.println("1.User Registration Sucessfull.");
 	}
-	@Test(priority=2)
+	@Test(dependsOnMethods="registration")
 	public void loginUser() {
 		System.out.println("2.User Login Sucessfull.");
 	}
-	@Test(priority=3)
+	@Test(dependsOnMethods="loginUser")
 	public void searchProducts() {
 		System.out.println("3.Seraching Products.");
 		System.out.println(1/0);//Exception
 	}
-	@Test(priority=4)
+	@Test(dependsOnMethods="searchProducts")
 	public void addToCart() {
 		System.out.println("4.Product Added to cart");
 	}
-	@Test(priority=5)
+	@Test(dependsOnMethods="addToCart")
 	public void orderTheProduct() {
 		System.out.println("5.Order placed.");
 	}

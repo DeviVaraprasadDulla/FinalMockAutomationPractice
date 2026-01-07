1cd11cf250ec0556561a2331c1fab72e2a9f80f2
Devi varaprasad Dulla <devivaraprasaddulla@gmail.com>
2026-01-07 15:55:03 +0530

    data provider

--- src/test/java/com/practice/TestNg/D_DataProvider.java
+++ src/test/java/com/practice/TestNg/D_DataProvider.java
@@ -1,5 +1,39 @@
 package com.practice.TestNg;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class D_DataProvider {
+	
+	
+	@DataProvider(name="users_Details")
+	public Object dataProviderMethod() {
+		
+		Object data[][] = new Object[3][2];
+		
+		data[0][0]="Prasad";
+		data[0][1]="Prasad@51";
+		
+		data[1][0]="Jhon";
+		data[1][1]="Jhon@2002";
+		
+		data[2][0] ="Steve";
+		data[2][1] ="Steve@02";
+		
+		return data;
+	}
+	
+	
+	@Test(dataProvider= "users_Details")
+	public void userLoging(String userName,String password) {
+		System.out.println("====== User Details ======");
+		System.out.println();
+		System.out.println("User Name : "+userName);
+		System.out.println("Password : "+password);
+		System.out.println();
+		System.out.println("=========================");
+		System.out.println();
+		System.out.println();
+	}
 
 }
